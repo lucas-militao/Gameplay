e15eda6a8c814ed609851a32ab6b9fd1ef7713e6
lumilitas <lucasmengineer@edu.unifor.br>
2021-06-21 11:11:56 -0300

    Implementada a splash screen

--- App.tsx
+++ App.tsx
@@ -1,21 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, Text } from "react-native";
+import { SignIn } from './src/screens/SignIn';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World, NLW Together!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SignIn />
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
