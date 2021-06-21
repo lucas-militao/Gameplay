dc76070a73d9ed4b0e272f59d1a5dfe15c570c5d
lumilitas <lucasmengineer@edu.unifor.br>
2021-06-21 17:08:14 -0300

    Implementada a página de SignIn

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,24 +1,48 @@
 import React from "react";
-import { useState } from "react";
-import { View, Text, TextInput } from "react-native";
+import { View, 
+         Text, 
+         Image,
+         StatusBar
+} from "react-native";
+import { ButtonIcon } from "../../components/ButtonIcon";
 
+import IllustrationImg from '../../assets/illustration.png';
 import { styles } from './styles'
 
 export function SignIn() {
-  const [text, setText] = useState('');
 
   return (
     <View style={ styles.container }>
-      <Text>Hello World, NLW Together!</Text>
-
-      <TextInput 
-        style={ styles.input }
-        onChangeText={ setText }  
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Image 
+        source={IllustrationImg} 
+        style={styles.image}
+        resizeMode="stretch"
       />
 
-      <Text>
-        Você digitou: { text }
-      </Text>
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Conecte-se {'\n'}
+          e organize suas {'\n'}
+          jogatinas {'\n'}
+        </Text>
+
+        <Text style={styles.subtitle}>
+          Crie grupos para jogar seus games {'\n'}
+          favoritos com seus amigos {'\n'}
+        </Text>
+
+        <ButtonIcon 
+          title="Entrar com Discord"
+          activeOpacity={0.7}
+        />
+
+      </View>
+
     </View>
   )
 }
