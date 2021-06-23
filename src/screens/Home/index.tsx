73e1de2efa7bec15145e17765670ca786c4d367c
lumilitas <lucasmengineer@edu.unifor.br>
2021-06-23 07:15:38 -0300

    Ajuste na transição da navegação

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { View, Text, FlatList } from "react-native";
 
+import { Background } from "../../components/Background";
 import { CategorySelect } from "../../components/CategorySelect";
 import { ButtonAdd } from "../../components/ButtonAdd";
 import { Profile } from "../../components/Profile";
@@ -47,36 +48,38 @@ export function Home() {
   }
 
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Profile />
-        <ButtonAdd />
-      </View>
+    <Background>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Profile />
+          <ButtonAdd />
+        </View>
 
-      <View>
-        <CategorySelect
-          categorySelected={category}
-          setCategory={handleCategorySelect}
-        />
-
-        <View style={styles.content}>
-          <ListHeader
-            title='Partidas Agendadas'
-            subtitle='Total 6'
+        <View>
+          <CategorySelect
+            categorySelected={category}
+            setCategory={handleCategorySelect}
           />
 
-          <FlatList
-            data={appointments}
-            keyExtractor={item => item.id}
-            renderItem={({item}) => (
-              <Appointment data={item} />
-            )}
-            ItemSeparatorComponent={() => <ListDivider />}
-            style={styles.matches}
-            showsVerticalScrollIndicator={false}
-          />
+          <View style={styles.content}>
+            <ListHeader
+              title='Partidas Agendadas'
+              subtitle='Total 6'
+            />
+
+            <FlatList
+              data={appointments}
+              keyExtractor={item => item.id}
+              renderItem={({item}) => (
+                <Appointment data={item} />
+              )}
+              ItemSeparatorComponent={() => <ListDivider />}
+              style={styles.matches}
+              showsVerticalScrollIndicator={false}
+            />
+          </View>
         </View>
       </View>
-    </View>
+    </Background>
   );
 }
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -6,6 +6,7 @@ import { View,
 import { useNavigation } from "@react-navigation/native";
 
 import { ButtonIcon } from "../../components/ButtonIcon";
+import { Background } from "../../components/Background";
 
 import IllustrationImg from '../../assets/illustration.png';
 import { styles } from './styles'
@@ -18,33 +19,33 @@ export function SignIn() {
   }
 
   return (
-    <View style={ styles.container }>
-      
-      <Image 
-        source={IllustrationImg} 
-        style={styles.image}
-        resizeMode="stretch"
-      />
-
-      <View style={styles.content}>
-        <Text style={styles.title}>
-          Conecte-se {'\n'}
-          e organize suas {'\n'}
-          jogatinas {'\n'}
-        </Text>
-
-        <Text style={styles.subtitle}>
-          Crie grupos para jogar seus games {'\n'}
-          favoritos com seus amigos {'\n'}
-        </Text>
-
-        <ButtonIcon 
-          title="Entrar com Discord"
-          onPress={handleSignIn}
+    <Background>
+      <View style={ styles.container }>    
+        <Image 
+          source={IllustrationImg} 
+          style={styles.image}
+          resizeMode="stretch"
         />
 
-      </View>
+        <View style={styles.content}>
+          <Text style={styles.title}>
+            Conecte-se {'\n'}
+            e organize suas {'\n'}
+            jogatinas {'\n'}
+          </Text>
 
-    </View>
+          <Text style={styles.subtitle}>
+            Crie grupos para jogar seus games {'\n'}
+            favoritos com seus amigos {'\n'}
+          </Text>
+
+          <ButtonIcon 
+            title="Entrar com Discord"
+            onPress={handleSignIn}
+          />
+        </View>
+      </View>
+    </Background>
+
   )
 }
